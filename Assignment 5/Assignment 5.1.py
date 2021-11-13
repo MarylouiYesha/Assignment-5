238aa16b538aeab1853c2d2c987c94ea5658a33e
user <yesha.maryloui@gmail.com>
2021-11-14 02:30:00 +0800

    updating files

--- Assignment 5/Assignment 5.1.py
+++ Assignment 5/Assignment 5.1.py
@@ -1,34 +1,34 @@
-your_grade=float(input("Enter your grade:"))
-
+import math
+grade=(float(input("Enter your grade:")))
+your_grade=math.ceil(grade)
 if your_grade>= 97 and your_grade<= 100:
         print("Grade/Mark:1.0")
         print("Description: Exellent")
-elif your_grade >=94 and your_grade<= 96:
+elif your_grade >=94 and your_grade<= 96.4:
         print("Grade/Mark:1.25")
         print("Description: Exellent")
-elif your_grade >=91 and your_grade<=93:
+elif your_grade >=91 and your_grade<=93.4:
         print("Grade/Mark:1.5")
         print("Description: Very Good")
-elif your_grade >=88 and your_grade<=90:
+elif your_grade >=88 and your_grade<=90.4:
         print("Grade/Mark:1.75")
         print("Description: Very Good")
-elif your_grade >=85 and your_grade<=87:
+elif your_grade >=85 and your_grade<=87.4:
         print("Grade/Mark:2.0")
         print("Description: Good")
-elif your_grade >=82 and your_grade<=84:
+elif your_grade >=82 and your_grade<=84.4:
         print("Grade/Mark:2.25")
         print("Description: Good")
-elif your_grade >=79 and your_grade<=81:
+elif your_grade >=79 and your_grade<=81.4:
         print("Grade/Mark:2.5")
         print("Description: Good")
-elif your_grade >=76 and your_grade<=78:
+elif your_grade >=76 and your_grade<=78.4:
         print("Grade/Mark:2.75")
         print("Description: Satisfactory")
 elif your_grade ==75:
         print("Grade/Mark:3.0")
         print("Description: Passing")
-elif your_grade >=65 and your_grade<=74:
+elif your_grade >=65 and your_grade<=74.4:
         print("Grade/Mark:5.0")
         print("Description: Failure")
-
     
