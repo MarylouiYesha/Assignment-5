bfa3667190905e6a731bf8ed733293bb5c1c70e4
user <yesha.maryloui@gmail.com>
2021-11-20 15:38:27 +0800

    Edited file

--- Assignment 5/Assignment 5.1.py
+++ Assignment 5/Assignment 5.1.py
@@ -1,46 +1,47 @@
 import math
-agrade=(input("Enter your grade:\n")) 
-if agrade == "Inc.":
+your_grade=(input("Enter your grade:\n")) 
+
+if your_grade == "Inc.":
         print("Grade/Mark: Inc.")
         print("Description: Incomplete")
-elif agrade == "W":
+elif your_grade == "W":
         print("Grade/Mark: W")
         print("Description: Withdrawn")
-elif agrade == "D":
+elif your_grade == "D":
         print("Grade/Mark: D")
         print("Description: Dropped")
-
-your_grade=(float(input("Enter your grade:")))
-
-if your_grade>= 97 and your_grade<= 100:
-          print("Grade/Mark:1.0")
-          print("Description: Exellent")
-elif your_grade >=94 and your_grade<= 96.4:
+else:
+        your_grade=float(your_grade)
+        if int(your_grade+0.5)>= 97 and int(your_grade+0.5)<= 100:
+         print("Grade/Mark:1.0")
+         print("Description: Exellent")
+        elif int(your_grade+0.5) >=94 and int(your_grade+0.5)<= 96:        
          print("Grade/Mark:1.25")
          print("Description: Exellent")
-elif your_grade >=91 and your_grade<=93.4:
+        elif int(your_grade+0.5) >=91 and int(your_grade+0.5)<=93:
          print("Grade/Mark:1.5")
          print("Description: Very Good")
-elif your_grade >=88 and your_grade<=90.4:
+        elif int(your_grade+0.5) >=88 and int(your_grade+0.5)<=90:
          print("Grade/Mark:1.75")
          print("Description: Very Good")
-elif your_grade >=85 and your_grade<=87.4:
+        elif int(your_grade+0.5) >=85 and int(your_grade+0.5)<=87:
          print("Grade/Mark:2.0")
          print("Description: Good")
-elif your_grade >=82 and your_grade<=84.4:
+        elif int(your_grade+0.5) >=82 and int(your_grade+0.5)<=84:
          print("Grade/Mark:2.25")
          print("Description: Good")
-elif your_grade >=79 and your_grade<=81.4:
+        elif int(your_grade+0.5) >=79 and int(your_grade+0.5)<=81:
          print("Grade/Mark:2.5")
          print("Description: Good")
-elif your_grade >=76 and your_grade<=78.4:
+        elif int(your_grade+0.5) >=76 and int(your_grade+0.5)<=78:
          print("Grade/Mark:2.75")
          print("Description: Satisfactory")
-elif your_grade ==75:
+        elif your_grade ==75:
          print("Grade/Mark:3.0")
          print("Description: Passing")
-elif your_grade >=65 and your_grade<=74.4:
+        elif int(your_grade+0.5) >=65 and int(your_grade+0.5)<=74:
          print("Grade/Mark:5.0")
          print("Description: Failure")
+        
 
 
